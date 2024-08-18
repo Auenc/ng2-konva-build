942b7c378ca7962c3d54b8a8347bb9016e1a0c12
Lewis <ljoncb@gmail.com>
2024-08-18 16:08:34 +0100

    moved angular to 18.1

--- ng2-konva/lib/utils/applyNodeProps.d.ts
+++
@@ -1,3 +0,0 @@
-import { KonvaComponent } from '../interfaces/ko-component.interface';
-import { PropsType } from './types';
-export default function applyNodeProps(component: KonvaComponent, props?: PropsType, oldProps?: PropsType): void;
--- ng2-konva/lib/utils/updatePicture.d.ts
+++
@@ -1,3 +0,0 @@
-import Konva from 'konva';
-import Node = Konva.Node;
-export default function updatePicture(node: Node): void;
